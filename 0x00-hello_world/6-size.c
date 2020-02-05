467419a4524a2aa4caeed37fe50d37471b1996ec
DRCCODES <1723@holbertonschool.com>
2020-02-05 20:12:15 +0000

    sizes of types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ * main - Print out size of type in bytes
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+
+	char charType;
+	int intType;
+	long longintType;
+	long long longlongintType;
+	float floatType;
+
+	printf("Size of a char: %ld byte(s)\n", sizeof(charType));
+	printf("Size of an int: %ld byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(longlongintType));
+	printf("Size of a float: %ld byte(s)\n", sizeof(floatType));
+
+	return (0);
+}
