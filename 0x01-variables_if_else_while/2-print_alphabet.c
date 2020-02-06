5970940e4d61758ef22dadf2579c22716f7349e3
DRCCODES <1723@holbertonschool.com>
2020-02-06 20:28:58 +0000

    puts a to z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Point Of Entry
+ *
+ *Description: Program prints lowercase a-z
+ *
+ *Return: Returns 0 upon success
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+		putchar(ch);
+
+	putchar('\n')
+	return(0)
+}
