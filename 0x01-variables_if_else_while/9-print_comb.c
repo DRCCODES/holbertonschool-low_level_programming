a145e612c08c1f59a12e9baa583fe2350098a7cc
DRCCODES <1723@holbertonschool.com>
2020-02-06 23:22:38 +0000

    while print 0-9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Point Of Entry
+ *
+ *Description: Program prints 0-9 wiht ,s
+ *
+ *Return: Returns 0 upon success
+ */
+int main(void)
+{
+	int n = '0';
+
+	while (n != '9')
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+		n++;
+		if (n == '9')
+		{
+			putchar(n);
+			putchar(' ');
+		}
+	}
+}
