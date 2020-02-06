9dcfdcf473e73ddbdfbfd5a3d26074150a2d6bc1
DRCCODES <1723@holbertonschool.com>
2020-02-06 20:39:19 +0000

    a-z and A-Z

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *main - Point Of Entry
+ *
+ *Description: Program prints  a-z then A-Z
+ *
+ *Return: Returns 0 upon success
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+		putchar(ch);
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
+		putchar(ch)
+
+	putchar('\n');
+
+	return (0);
+}
