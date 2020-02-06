06ed6123e1658cd08c1e8f1445666bf1e0845f95
DRCCODES <1723@holbertonschool.com>
2020-02-06 20:53:33 +0000

    no q no e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ *main - Point Of Entry
+ *
+ *Description: Program prints every letter a-z save q & e
+ *
+ *Return: Returns 0 upon success
+ */
+
+int main(void)
+{
+
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+		if (ch == 'e' || ch == 'q')
+			ch++;
+		else
+			putchar(ch);
+}
+	putchar('n\');
+	return (0);
