f3073091d1ae9df7840a422c84ec17e602616c27
DRCCODES <1723@holbertonschool.com>
2020-02-06 23:44:27 +0000

    fixed formating and missing ;

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,16 +13,19 @@ int main(void)
 {
 	int n = '0';
 
-	while (n != '9')
+	while (n <= '9')
 	{
-		putchar(n);
-		putchar(',');
-		putchar(' ');
-		n++;
-		if (n == '9')
+		putchar (n);
+
+
+		if (n != '9')
 		{
-			putchar(n);
-			putchar(' ');
+			putchar (',')
+			putchar (' ');
+
 		}
+		n++
 	}
+	putchar ('n');
+	return (0);
 }
