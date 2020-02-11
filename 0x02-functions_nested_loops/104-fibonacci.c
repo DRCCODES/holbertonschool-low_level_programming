5ac0f856a14efd9aa17959a66a840beedadf51ee
DRCCODES <1723@holbertonschool.com>
2020-02-11 23:18:23 +0000

    reworked 103 to to 104

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main- print up to 98th term sarting 1 ,2
+ *
+ *Return: 0
+ */
+
+int main(void)
+{
+double i;
+double j = 1;
+double k = 2;
+double c = 0;
+
+for (i = 0; i < 98; i++)
+{
+c = (j + k);
+k = j;
+j = c;
+printf("%f, ", c);
+}
+return (0);
