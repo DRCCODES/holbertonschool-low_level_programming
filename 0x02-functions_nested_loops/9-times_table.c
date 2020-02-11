a9cbd4a9db6416fa808821b2b8629f612cd35045
DRCCODES <1723@holbertonschool.com>
2020-02-11 16:48:39 +0000

    corrected extra ' ' before leading 0

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,14 +18,14 @@ void times_table(void)
 		for (j = 0; j < 10 ; j++)
 		{
 		k = i * j;
-		if ((k) < 10)
+		if (j == 0)
+		{
+		_putchar ('0' + k);
+		}
+		else if ((k) < 10)
 		{
 			_putchar(' ');
 			_putchar('0' + ((k) % 10));
-		}
-		else if (j == 0)
-		{
-		_putchar('0' + k);
 		}
 		else
 		{
