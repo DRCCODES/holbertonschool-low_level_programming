1c7e087c4846ba17890bfce3278347310797b211
DRCCODES <1723@holbertonschool.com>
2020-02-11 15:17:01 +0000

    added missing ; after return

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -13,7 +13,7 @@ int main(void)
 {
 	_putchar('H');
 	_putchar('o');
-	_putchar('l');
+	_putchar('i');
 	_putchar('b');
 	_putchar('e');
 	_putchar('r');
@@ -21,6 +21,7 @@ int main(void)
 	_putchar('o');
 	_putchar('n');
 	_putchar('\n');
-	return (0)
+
+	return (0);
 
 }
