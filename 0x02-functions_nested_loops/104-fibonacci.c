39869b0e04926753e618c231a5fce240d8ac5e7f
DRCCODES <1723@holbertonschool.com>
2020-02-13 03:47:56 +0000

    numbers last digits right

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,24 +1,48 @@
 #include <stdio.h>
 /**
- *main- print up to 98th term sarting 1 ,2
+ *main- prints first 98 fib terms
  *
  *Return: 0
  */
-
 int main(void)
 {
-unsigned long int i;
-unsigned long int  j = 1;
-unsigned long int  k = 2;
-unsigned long int  c = 0;
-printf("%lu, ", j);
-printf("%lu, ", k);
-for (i = 0; i < 96; i++)
+int i = 0;
+unsigned long int tr1 = 1, tr2 = 2, nT, b1, b2, b3, b4, b5, b6;
+nT = tr2 + tr1;
+printf("%lu, ", tr1);
+printf("%lu, ", tr2);
+while (i < 90)
 {
-c = (j + k);
-j = k;
-k = c;
-printf("%lu, ", c);
+printf("%lu, ", nT);
+tr1 = tr2;
+tr2 = nT;
+nT = tr1 + tr2;
+i++;
+}
+b5 = tr1 / 1000000000;
+b4 = tr1 % 1000000000;
+b3 = tr2 / 1000000000;
+b2 = tr2 % 1000000000;
+while (i >= 90 && i < 95)
+{
+if (i < 94)
+{
+b1 = b5 + b3;
+b6 = b4 + b2;
+printf("%lu%lu, ", b1, b6);
+b5 = b3;
+b4 = b2;
+b3 = b1;
+b2 = b6;
+i++;
+}
+else
+{
+b1 = b5 + b3;
+b6 = b4 + b2;
+printf("%lu%lu", b1, b6);
+i++;
+}
 }
 printf("\n");
 return (0);
