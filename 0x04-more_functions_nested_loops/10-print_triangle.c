f6964b99e57cd3894e30ab242893bbbb3ebefba0
DRCCODES <1723@holbertonschool.com>
2020-02-13 20:21:05 +0000

    triangle right shape now

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,19 +9,25 @@
 
 void print_triangle(int size)
 {
-{
 int x, y;
 if (size > 0)
 {
-for (y = 0; y < size; y++)
+for (y = 1; y <= size; y++)
 {
-for (x = 0; x <= y; x++)
+for (x = 1; x <= size; x++)
+{
+if (x <= size - y)
+{
+_putchar(' ');
+}
+else
 {
 _putchar('*');
 }
-_putchar('\n');
-}
 }
 _putchar('\n');
 }
+}
+else
+_putchar('\n');
 }
