ffa3344894d6d304c2ae82be088228622e8659d9
DRCCODES <1723@holbertonschool.com>
2020-02-13 17:00:19 +0000

    added more_numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - prints 1-14 ten times.
+ *
+ * Returns: Nothing
+ */
+
+void more_numbers(void)
+{
+	int i;
+	int x;
+
+	while (x < 10)
+	{
+	for (i = 0; i < 15; i++)
+	{
+	if (i >= 10)
+	{
+	_putchar('0' + (i / 10));
+
+	}
+	_putchar('0' + (i % 10));
+
+	}
+	x++;
+	_putchar('\n');
+}
