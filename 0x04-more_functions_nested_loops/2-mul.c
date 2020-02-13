2e3e3cb42d5740ff3d05a9f5fd7a0c265d3e9538
DRCCODES <1723@holbertonschool.com>
2020-02-13 16:06:45 +0000

    multiplication function added

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * mul - multiplies two numbers returns results
+ * @a: - first number
+ * @b: - second number
+ * Return: results of multiplication
+ */
+
+int mul(int a, int b)
+{
+
+	int num = a * b;
+
+	return (num);
+
+}
