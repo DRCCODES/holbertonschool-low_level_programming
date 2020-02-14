82078ca4b9368a9ca62b092524bc56804cddb7da
DRCCODES <1723@holbertonschool.com>
2020-02-14 03:27:43 +0000

    Prime Factor Largest

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+
+#include <stdio.h>
+
+/**
+  * main - Entry Point
+  &
+  * Description: Finds largest prime factor by divison
+  *
+  * Return: 0
+  */
+
+int main(void)
+{
+	long int d, n, lp;
+
+	n = 612852475143;
+
+	for (d = 2; d < n; d++)
+	{
+	while (n % d == 0)
+	{
+	n = n / d;
+	lp = n;
+	}
+	}
+	printf("%ld\n", lp);
+	return (0);
+}
