18173c2a2af76f58321913493b74ed8040160771
DRCCODES <1723@holbertonschool.com>
2020-02-18 17:09:01 +0000

    added puts_half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,41 @@
+#include "holberton.h"
+
+/**
+ * puts_half - prints out last half of string
+ *
+ * @str: string of characters to be printed
+ *
+ * Return: N/A
+ */
+
+void puts_half(char *str)
+{
+	int i;
+	int j;
+
+	while (str[i] != '\0')
+	{
+	i++;
+	}
+
+	if (i % 2 != 0 && i > 1)
+	{
+	j = (i - 1);
+	j = j / 2;
+	}
+	else if (i % 2 == 0 && i > 1)
+	{
+	j = i / 2;
+	}
+	else
+	{
+	j = 0;
+	}
+	while (j < i)
+	{
+	_putchar(str[j]);
+	j++;
+
+	}
+_putchar('\n');
+}
