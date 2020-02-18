cf489e1b9d1ab853e8394d632752240fc8e58d5d
DRCCODES <1723@holbertonschool.com>
2020-02-18 16:25:25 +0000

    removed char *n

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,7 +10,7 @@
 
 void print_rev(char *s)
 {
-	char *n = s;
+
 	int i = 0;
 
 	while (s[i] != '\0')
@@ -19,7 +19,8 @@ void print_rev(char *s)
 	}
 	while (i >= 0)
 	{
-	_putchar(n[i]);
+	i = i - 1;
+	_putchar(s[i]);
 	i--;
 	}
 	_putchar('\n');
