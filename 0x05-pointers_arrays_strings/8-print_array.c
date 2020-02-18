70191d6266d097bc53e38c4efd298fe5787e3cdb
DRCCODES <1723@holbertonschool.com>
2020-02-18 19:13:28 +0000

    trying for loop

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -15,11 +15,14 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	while (i < n - 1)
+	for (i = 0; i < n; i++)
 	{
-	printf("%d, ", a[i]);
-	i++;
-	}
 	printf("%d", a[i]);
+	if (i != n - 1)
+	{
+	printf(", ");
+	}
+
 	printf("\n");
+
 }
