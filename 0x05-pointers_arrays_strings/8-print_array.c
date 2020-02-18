da95f495b40fcb0b5905b3ef1adfa6e5bb5dbbd0
DRCCODES <1723@holbertonschool.com>
2020-02-18 18:33:14 +0000

    added print_array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_array - print an array
+ *
+ * @a: array to draw from
+ *
+ * @n: index to print
+ *
+ * Return: N/A
+ */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n - 1)
+	{
+	printf("%d, ", a[i]);
+	i++;
+	}
+	printf("%d", a[i]);
+	_putchar('\n');
+}
