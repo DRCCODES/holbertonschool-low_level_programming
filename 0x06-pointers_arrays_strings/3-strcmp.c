947ada28157b97e34fc4d9db15f121d23736215d
DRCCODES <1723@holbertonschool.com>
2020-02-20 18:58:45 +0000

    fixed compile issue

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,7 +15,16 @@ int _strcmp(char *s1, char *s2)
 {
 int i;
 for (i = 0; s1[i] == s2[i]; i++)
-if (s1[i] == '\0')
+{
+	if (s1[i] == '\0')
+	{
+		break;
+	}
+	else
+	{
+		i++;
+	}
+}
 
 return (s1[i] - s2[i]);
 }
