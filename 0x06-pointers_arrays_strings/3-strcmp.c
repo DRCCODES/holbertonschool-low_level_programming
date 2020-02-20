d1c648c2c381a9f841019be47d842be54f22ff26
DRCCODES <1723@holbertonschool.com>
2020-02-20 17:46:51 +0000

    added strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _strcmp - compares strs like library strcmp
+ *
+ * @s1: main str compared too
+ *
+ * @s2: str compard to s1
+ *
+ * Return: s1 - s2
+ *
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+int i;
+for (i = 0; s1[i] == s2[i]; i++)
+if (s1[i] == '\0')
+
+return (s1[i] - s2[i]);
+}
