2e889ccf1d7fa619c0b33b7da8107538fc6e16f0
DRCCODES <1723@holbertonschool.com>
2020-02-24 23:55:50 +0000

    much simpler than thought

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * set_string - make one string another
+ * @s: the str to be replaced
+ * @to: str to be copied
+ * Return: N/A
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
