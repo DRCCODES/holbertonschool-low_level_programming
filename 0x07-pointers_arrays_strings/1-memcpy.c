148cb14e07580667af75c18e45675906f16aeb3c
DRCCODES <1723@holbertonschool.com>
2020-02-24 16:03:53 +0000

    added memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+/**
+ * _memcpy -copies using pointers
+ * @dest: where to copy to
+ * @n: the size of the memory to print
+ * @src: source to copy from
+ * Return: Nothing.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+char *csrc = (char *)src;
+char *cdest = (char *)dest;
+for (int i = 0; i < n ; i++)
+cdest[i] = csrc[i];
+return (dest);
+}
