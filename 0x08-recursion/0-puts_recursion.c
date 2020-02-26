c6e1f00e95b4d7e301922b02a33a54f3347980e3
DRCCODES <1723@holbertonschool.com>
2020-02-26 17:52:16 +0000

    added puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - print out str using recursion
+ * @s: str to be printed
+ *
+ * Return: N/A
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+_putchar(*s);
+s++;
+if (*s != '\0')
+{
+_puts_recursion(s);
+}
+}
