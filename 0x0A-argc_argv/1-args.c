f4644d1a0f86ef9d23f5fffc28c29d3a7f4b7398
DRCCODES <1723@holbertonschool.com>
2020-02-29 23:46:51 +0000

    adding args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+/**
+  * main - print num of arguments
+  * @argc: the count
+  * @argv: the array
+  * Return: 0
+  */
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	printf("argc = %d\n", argc);
+	return (0);
+}
