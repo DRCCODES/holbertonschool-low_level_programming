0729f5bf7ec138ebfec72669a153ec52afcec62b
DRCCODES <1723@holbertonschool.com>
2020-03-01 21:46:43 +0000

    redid in emacs

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
 int check_D(char *s);
 /**
  * main - add numbers if positive
@@ -13,34 +12,32 @@ int check_D(char *s);
 
 int main(int argc, char *argv[])
 {
-	int i;
-	int sum = 0;
+int i;
+long sum;
 
-	if (argc < 2)
-	{
-		printf("%d\n", 0);
-		return (1);
-	}
-	for (i = 1, sum = 0; i < argc; i++)
-	{
-
-		if (!check_D(argv[i]))
-		{
-			printf("Error\n");
-			return (1);
-		}
-		sum += atoi(argv[i]);
-	}
-
-	printf("%d\n", sum);
-	return (0);
+if (argc < 2)
+{
+printf("%d\n", 0);
+return (1);
+}
+for (i = 1, sum = 0; i < argc; i++)
+{
+if (!check_D(argv[i]))
+{
+printf("Error\n");
+return (1);
+}
+sum += atoi(argv[i]);
+}
+printf("%ld\n", sum);
+return (0);
 }
 
 /**
-  * check_D - checks substr for nondigit
-  * @s: the substring
-  * Return: 0 or 1
-  */
+ * check_D - checks substr for nondigit
+ * @s: the substring
+ * Return: 0 or 1
+ */
 
 int check_D(char *s)
 {
