da6908ce9eb492fa4518ee47a0af301d111710cc
DRCCODES <1723@holbertonschool.com>
2020-03-05 23:48:45 +0000

    testing code for _realloc.c

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,45 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _realloc - realloc memory
+ * @ptr: the pointer
+ * @old_size: the old size of pointer
+ * @new_size: new size to allot
+ * Return: NULL,ptr and or new_size
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+void *ptrNew;
+
+if (new_size == 0)
+{
+free(ptr);
+return (NULL);
+}
+else if (!ptr)
+{
+return (malloc(new_size));
+}
+else if (new_size <= old_size)
+{
+return (ptr);
+}
+else
+{
+
+if ((ptr) && (new_size > old_size))
+ptrNew = malloc(new_size);
+
+if (ptrNew)
+{
+memcpy(ptrNew, ptr, old_size);
+free(ptr);
+}
+return (ptrNew);
+}
+}
