3885189ad1a26faa110d956a4f578cb1ef9a50af
DRCCODES <1723@holbertonschool.com>
2020-03-05 18:40:33 +0000

    added 2-colloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -14,17 +14,18 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 
 	int *p;
+	unsigned int i;
 
-	if (nmemb == 0 || size == 0)
-	return (NULL);
+	if (nmemb <= 0 || size <= 0)
+		return (NULL);
 
 	p = malloc(nmemb * size);
 
 	if (p == NULL)
-	return (NULL);
+		return (NULL);
 
 	for (i = 0; i < nmemb; i++)
-	p[i] = 0;
+		p[i] = 0;
 
 	return (p);
 }
