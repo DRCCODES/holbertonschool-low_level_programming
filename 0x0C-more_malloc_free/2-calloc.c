2881ce3431ca8c6f7e74090f8d9854cf6e7ea99a
DRCCODES <1723@holbertonschool.com>
2020-03-05 18:30:42 +0000

    added 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for array
+ * @nmemb: num of elements
+ * @size: size of elements
+ *
+ * Return: Nothing.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+	int *p;
+
+	if (nmemb == 0 || size == 0)
+	return (NULL);
+
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+	return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+	p[i] = 0;
+
+	return (p);
+}
