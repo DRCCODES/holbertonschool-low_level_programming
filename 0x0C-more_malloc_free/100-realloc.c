d6076a504fc7a6a9d6672618a8ad243972551d91
DRCCODES <1723@holbertonschool.com>
2020-03-06 02:23:16 +0000

    added _memcpy and adjusted logic

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -4,6 +4,24 @@
 #include <string.h>
 
 /**
+ * _memcpy -copies using pointers
+ * @dest: where to copy to
+ * @n: the size of the memory to print
+ * @src: source to copy from
+ * Return: Nothing.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+char *csrc = (char *)src;
+char *cdest = (char *)dest;
+for (i = 0; i < n ; i++)
+cdest[i] = csrc[i];
+return (dest);
+}
+
+/**
  * _realloc - realloc memory
  * @ptr: the pointer
  * @old_size: the old size of pointer
@@ -13,11 +31,9 @@
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-
 void *ptrNew;
-
 if (ptr == NULL)
-	{
+{
 ptrNew = malloc(new_size);
 if (ptrNew == NULL)
 {
@@ -27,30 +43,22 @@ return (NULL);
 free(ptr);
 return (ptrNew);
 }
-if (new_size == 0)
+if (new_size == old_size)
+return (ptr);
+if (new_size == 0 && ptr != NULL)
 {
 free(ptr);
 return (NULL);
 }
-else if (!ptr)
-{
-return (malloc(new_size));
-}
-else if (new_size == old_size)
-{
-return (ptr);
-}
-else
-{
-
-if ((ptr) && (new_size > old_size))
 ptrNew = malloc(new_size);
-
-if (ptrNew)
+if (ptrNew == NULL)
 {
-memcpy(ptrNew, ptr, old_size);
 free(ptr);
+return (NULL);
 }
+if (new_size > old_size)
+_memcpy(ptrNew, ptr, old_size);
+free(ptr);
 return (ptrNew);
 }
-}
+
