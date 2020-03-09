1ccfe2f19b3db1c7a3da39c455756ba661911edf
DRCCODES <1723@holbertonschool.com>
2020-03-09 17:50:02 +0000

    __FILE__

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Definition: Prints name of file compiled from
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
