a31f0e1f64e6b16cba542d50c06b57fd170773f7
DRCCODES <1723@holbertonschool.com>
2020-03-09 18:35:30 +0000

    dog_e added

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+/**
+ * struct dog - dogs attributes defined
+ * @name: dog name str
+ * @age: float age of dog
+ * @owner: owner name str
+ */
+
+
+typedef struct dog 
+{
+
+	char *name;
+	float age;
+	char *owner;
+} dog_e;
+
+#endif
