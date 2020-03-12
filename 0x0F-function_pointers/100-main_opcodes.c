b1f0f8535e8f8b05633241d77c63e4fa4d49e2b9
DRCCODES <1723@holbertonschool.com>
2020-03-12 03:33:36 +0000

    added opc

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - entry point
+  * @argc: counter of arguments
+  * @argv: holds arguments
+  * Return: 0 or hex values
+  */
+
+int main(int argc, char *argv[])
+{
+	int opn;
+	int i;
+	char *ptr = (char *)main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	opn = atoi(argv[1]);
+
+	if (opn <0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (i = 0; i < opn; i++)
+	{
+		printf("%02hhx ",ptr[i]);
+	}
+	
+	printf("%02hhx\n", ptr[i]);
+	return (0);
+}
