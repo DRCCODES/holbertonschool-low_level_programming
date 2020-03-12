61ad4cc0bb53df062255687395b80b84bbc84469
DRCCODES <1723@holbertonschool.com>
2020-03-12 01:03:49 +0000

    added files for task 4 aka #3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,27 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - entrance of program
+ * @argc: arg counter
+ * @argv: holds arguments
+ * Return: the result of operation
+ **/
+int main(int argc, char *argv[])
+{
+	int num1;
+	int num2;
+	int op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = (*get_op_func(argv[2]))(num1, num2);
+	printf("%d\n", op);
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * op_add - adds two integers
+ * @a: num 1
+ * @b: num2
+ * Return: result
+ **/
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtractes two integers
+ * @a: num1
+ * @b: num2
+ * Return: result
+ **/
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplies two integers
+ * @a: num1
+ * @b: num2
+ * Return: result
+ **/
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - divid to integers
+ * @a: num1
+ * @b: num2
+ * Return: result
+ **/
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+/**
+ * op_mod - module of integer
+ * @a: num1
+ * @b: num2
+ * Return: result
+ **/
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
