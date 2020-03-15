4c03107cc2188bea413bc97154b71f8ee925e151
DRCCODES <1723@holbertonschool.com>
2020-03-15 00:11:26 +0000

    emacs instead of vim

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,23 +11,23 @@
 int sum_them_all(const unsigned int n, ...)
 {
 
-	va_list list;
-	int sum = 0;
-	unsigned int i;
+  va_list list;
+  int sum = 0;
+  unsigned int i;
 
-	va_start(list, n);
+  va_start(list, n);
 
-	if (n == 0)
-	{
-	return (0);
-	}
+  if (n == 0)
+    {
+      return (0);
+    }
 
-	for (i = 0; i < n; i++)
-	{
-		sum = sum + va_arg(list, int);
-	}
+  for (i = 0; i < n; i++)
+    {
+      sum = sum + va_arg(list, int);
+    }
 
-va_end(list);
+  va_end(list);
 
-return (sum);
+  return (sum);
 }
