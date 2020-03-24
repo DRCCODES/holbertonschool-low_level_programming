29544f666cdde1e8a9143c9dfe2606d88d35e19c
DRCCODES <1723@holbertonschool.com>
2020-03-24 16:56:02 +0000

    added print_listint

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+  * print_listint - prints the elements of a list
+  * @h: pointer that points to first element in list_t
+  * Return: node count
+  */
+size_t print_listint(const listint_t *h)
+{
+const listint_t  *current_node = h;
+int len = 0;
+
+while ( current_node != NULL) {
+printf("%d\n", current_node->n);
+current_node = current_node->next;
+len++;
+}
+return (len);
+}
