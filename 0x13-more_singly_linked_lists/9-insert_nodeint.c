90ac942d04168b01e366c52fd5532138d00922de
DRCCODES <1723@holbertonschool.com>
2020-03-24 22:12:06 +0000

    added insert_nodeint

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+  * insert_nodeint_at_index - insert node at given index
+  * @head: head of linked list
+  * @idx: index location to add new node
+  * @n: value to add for new node
+  * Return: return new node addess or NULL
+  */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *tmp;
+	listint_t *new;
+
+	tmp = *head;
+
+	new = malloc(sizeof(listint_t));
+
+	i = 0;
+	while (i != idx1)
+	{
+	if (tmp->next == NULL)
+		return (NULL);
+
+	if (i == idx)
+	{
+	new->n = n;
+	new->next = tmp->next;
+	tmp->next = new;
+	}
+	tmp = tmp->next;
+
+	i++;
+	}
+	return (new);
+}
