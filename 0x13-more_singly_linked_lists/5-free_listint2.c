38e776c8cb2896e5c5412c9f8dcabaef7c9231f9
DRCCODES <1723@holbertonschool.com>
2020-03-24 19:01:54 +0000

    no memory leaks

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/**
+  * free_listint2 - frees in list_t
+  * @head: pointer to first element
+  * Return: N/A
+  */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+
+	if (!head)
+		return;
+
+	while (*head !=NULL)
+	{	
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+}
