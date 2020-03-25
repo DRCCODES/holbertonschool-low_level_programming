41705ada5c3ae0a5cbab63e5b2c278115ce9ef40
DRCCODES <1723@holbertonschool.com>
2020-03-25 04:34:27 +0000

    adding strt of print listint safe

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint_safe - safely print linked list.
+ * @head: head of linked list
+ * Return: Return 0.
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	return (0);
+}
