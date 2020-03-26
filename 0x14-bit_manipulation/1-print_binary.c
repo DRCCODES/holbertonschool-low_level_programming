a393332d0e68ecaf1085ac9cf8f0bf2b1253ca5a
DRCCODES <1723@holbertonschool.com>
2020-03-26 21:14:06 +0000

    no idea what to set mask at, made recursive instead

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -3,34 +3,14 @@
 
 /**
  * print_binary - prints decimal num as binary
- * @num: number to print out in binary
+ * @n: number to print out in binary
  * Return: N/A
  */
 
-void print_binary(unsigned long int num)
+void print_binary(unsigned long int n)
 {
-	unsigned long  int mask = 32768;
-
-	int t = 0;
-
-	if (num == 0)
-	{
-	_putchar('0');
-	}
-	while (mask > 0)
-	{
-	if ((num & mask) == 0 && t != 1)
-	{
-	mask = mask >> 1;
-	continue;
-	}
-	if (num & mask)
-	{
-	t = 1;
-	_putchar('1');
-	}
-	else
-	_putchar('0');
-	mask = mask >> 1;
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
+}
 }
