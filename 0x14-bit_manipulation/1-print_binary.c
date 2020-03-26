0936591a4ec7af7b0d0c08cb508225ae166ce7aa
DRCCODES <1723@holbertonschool.com>
2020-03-26 19:46:49 +0000

    added print_binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_binary - prints decimal num as binary
+ * @num: number to print out in binary
+ * Return: N/A
+ */
+
+void print_binary(unsigned int num)
+{
+	unsigned long  int mask = 32768;
+
+	int t = 0;
+
+	if (num == 0)
+	{
+	_putchar('0');
+	}
+	while (mask > 0)
+	{
+	if ((num & mask) == 0 && t != 1)
+	{
+	mask = mask >> 1;
+	continue;
+	}
+	if (num & mask)
+	{
+	t = 1;
+	_putchar('1');
+	}
+	else
+	_putchar('0');
+	mask = mask >> 1;
+	}
+}
