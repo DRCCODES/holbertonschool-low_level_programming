9b384e46a020a0e2f33fbe65c75b36d92f2aed4a
DRCCODES <1723@holbertonschool.com>
2020-04-01 00:00:34 +0000

    added 3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,63 @@
+#include "holberton.h"
+
+/**
+  * main - entry point, copy file
+  * @argc: number of arguments
+  * @argv: pointer to the str
+  * Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	int fd1;
+	int fd2;
+	int target;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		exit(97);
+	}
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE");
+		exit(98);
+	}
+	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE");
+		exit(99);
+	}
+	while ((target = read(fd1, buf, 1024)) > 0)
+	{
+		if (target == -1)
+		{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+		}
+		target = write(fd2, buf, target);
+		if (target == -1)
+		{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+		}
+	}
+	if (target == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (close(fd1) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
+		exit(100);
+	}
+	if (close(fd2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+	return (0);
+}
