1323447565f4325beabf99e5ef574b88ca0fa06f
DRCCODES <1723@holbertonschool.com>
2020-04-01 00:19:55 +0000

    added \n to statments

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
   * main - entry point, copy file
   * @argc: number of arguments
@@ -8,26 +7,24 @@
   */
 int main(int argc, char *argv[])
 {
-	int fd1;
-	int fd2;
-	int target;
+	int fd1. fd2. target;
 	char buf[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE");
+		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
 		exit(98);
 	}
 	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE");
+		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
 		exit(99);
 	}
 	while ((target = read(fd1, buf, 1024)) > 0)
