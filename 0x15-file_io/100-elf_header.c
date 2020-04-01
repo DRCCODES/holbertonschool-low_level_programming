c6eb0da77232cc498b87eb06e6f01f9543cbb92b
DRCCODES <1723@holbertonschool.com>
2020-04-01 00:46:24 +0000

    pushing placeholder

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,14 @@
+#include holberton.h
+
+/**
+ * main - display information of ELF file header
+ * @argc: arguments
+ * @argv: string arguments
+ *
+ * Return: at somepint exit 98
+ */
+
+int main(int argc, char *argv[])
+{
+	exit(98);
+}
