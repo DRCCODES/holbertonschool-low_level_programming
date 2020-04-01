36e40edfee160ac963886d10c0202e765fb79888
DRCCODES <1723@holbertonschool.com>
2020-04-01 00:42:19 +0000

    fixed error output

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,8 @@
 #include "holberton.h"
+#define ERRUSE ("Usage: cp file_from file_to\n")
+#define ERREAD ("Error: Can't read from file %s\n")
+#define ERRWRITE ("Error: Can't write to %s\n")
+#define ERRCLOSE ("Error: Can't close fd %d\n")
 /**
   * main - entry point, copy file
   * @argc: number of arguments
@@ -11,50 +15,36 @@ int main(int argc, char *argv[])
 	char buf[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		dprintf(STDERR_FILENO, ERRUSE), exit(97);
 	fd1 = open(argv[1], O_RDONLY);
 	if (fd1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file NAME_OF_THE_FILE\n");
-		exit(98);
-	}
+		dprintf(STDERR_FILENO, ERREAD, argv[1]), exit(98);
 	fd2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd2 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to NAME_OF_THE_FILE\n");
-		exit(99);
-	}
+		dprintf(STDERR_FILENO, ERRWRITE, argv[2]), exit(99);
 	while ((target = read(fd1, buf, 1024)) > 0)
 	{
 		if (target == -1)
 		{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		dprintf(STDERR_FILENO, ERREAD, argv[1]), exit(98);
 		}
 		target = write(fd2, buf, target);
 		if (target == -1)
 		{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, ERRWRITE, argv[2]), exit(99);
 		}
 	}
 	if (target == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		dprintf(STDERR_FILENO, ERREAD, argv[1]), exit(98);
 	}
 	if (close(fd1) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd1);
-		exit(100);
+		dprintf(STDERR_FILENO, ERRCLOSE, fd1), exit(100);
 	}
 	if (close(fd2) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
-		exit(100);
+		dprintf(STDERR_FILENO, ERRCLOSE, fd2), exit(100);
 	}
 	return (0);
 }
