08e5f7e351fb79537b7375dd05a056d56b56505b
DRCCODES <1723@holbertonschool.com>
2020-05-02 23:05:10 +0000

    compiles again

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -40,7 +40,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *new;
 
 	if (h == NULL || (*h == NULL && idx > 0))
-		return (NULL)
+		return (NULL);
 
 	if (idx == 0)
 		return (add_dnodeint(h, n));
