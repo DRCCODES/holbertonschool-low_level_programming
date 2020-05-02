38d3bc2b1bb47d7e13f75d19d0b380620607968d
DRCCODES <1723@holbertonschool.com>
2020-05-02 23:03:14 +0000

    if statment checking NULL and idx

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -39,6 +39,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *tmp = *h;
 	dlistint_t *new;
 
+	if (h == NULL || (*h == NULL && idx > 0))
+		return (NULL)
+
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 
