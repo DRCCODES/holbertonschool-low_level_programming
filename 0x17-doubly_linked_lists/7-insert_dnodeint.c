f9e343e4e4ca8f4a1f99fcf41bd4ea70f943e67d
DRCCODES <1723@holbertonschool.com>
2020-05-02 20:06:06 +0000

    Added 7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,6 +1,31 @@
 #include "lists.h"
 
 /**
+ * dlistint_lenB - gets len of double linked list
+ * @h: pointer to head
+ *
+ *
+ * Return: returns size of list
+ */
+
+size_t dlistint_lenB(const dlistint_t *h)
+{
+int nodes = 0;
+
+if (h == NULL)
+{
+	return (0);
+}
+
+while (h != NULL)
+	{
+	nodes++;
+	h = h->next;
+	}
+return (nodes);
+}
+
+/**
   * insert_dnodeint_at_index - insert node at given index
   * @h: head of linked list
   * @idx: index location to add new node
@@ -10,18 +35,23 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	unsigned int i;
+	unsigned int i, len;
 	dlistint_t *tmp;
 	dlistint_t *new;
+
+	len = dlistint_lenB(*h);
 
 	if (h == NULL)
 		return (NULL);
 
 	tmp = *h;
 	new = malloc(sizeof(dlistint_t));
-	if (new == NULL)
+
+	if (idx > len)
 		return (NULL);
 
+	if (new == NULL)
+		return (NULL);
 	i = 0;
 
 	new->n = n;
@@ -33,17 +63,13 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		*h = new;
 		return (new);
 	}
-
-
 	while (i < idx - 1)
 	{
 	if (tmp == NULL)
 		return (NULL);
-
 	tmp = tmp->next;
 	i++;
 	}
-
 	new->next = tmp->next;
 	tmp->next = new;
 	return (new);
