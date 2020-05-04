5f1ec38cef0aa8772ba38a4cd80e0cfee75985db
DRCCODES <1723@holbertonschool.com>
2020-05-04 03:54:15 +0000

    place holder file

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+/**
+ * main - entry point of crackme
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: always 0
+ */
+
+
+int main(int argc, char **argv)
+{
+	return (0);
+}
