fcac3121bce9498be9d7355862200d0168bd3ea4
DRCCODES <1723@holbertonschool.com>
2020-05-29 19:48:51 +0000

    added task 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,42 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - prints hash table key values
+ * @ht: The Hash Table to print
+ * Return: N/A
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *pn;
+	unsigned long int i = 0;
+	int ns = 0;
+
+	if (ht == NULL)
+		return;
+
+	if (ht->size == 0)
+		return;
+
+	printf("{");
+
+	while (i < ht->size)
+	{
+		pn = ht->array[i];
+		while (pn != NULL)
+		{
+		if (ns == 0)
+		{
+			printf("'%s': '%s', ", pn->key, pn->value);
+			ns++;
+		}
+		else
+		{
+		printf("', %s': '%s'", pn->key, pn->value);
+		}
+		pn = pn->next;
+		}
+		i++;
+	}
+	printf("}\n");
+}
