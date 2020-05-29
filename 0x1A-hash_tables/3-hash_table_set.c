93bde904529dc8f697142f718096cb681a70e233
DRCCODES <1723@holbertonschool.com>
2020-05-29 18:44:28 +0000

    fixed task 3 formatting

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,45 +10,44 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-        unsigned long int idx;
-        hash_node_t *nn;
-        hash_node_t *tn;
+	unsigned long int idx;
+	hash_node_t *nn;
+	hash_node_t *tn;
 
-        if (ht == NULL)
-                return (0);
-        if (key == NULL)
-                return (0);
-        if (ht->size == 0)
-                return (0);
+	if (ht == NULL)
+		return (0);
+	if (key == NULL)
+		return (0);
+	if (ht->size == 0)
+		return (0);
 
-        idx = key_index((unsigned char *)key, ht->size);
+	idx = key_index((unsigned char *)key, ht->size);
+	tn = ht->array[idx];
 
-        tn = ht->array[idx];
+	while (tn != NULL)
+	{
+	if (strcmp(tn->key, key) == 0)
+	{
+		free(tn->value);
+		tn->value = strdup(value);
+		return (1);
+	}
+		tn = tn->next;
+	}
+	nn = malloc(sizeof(hash_node_t));
+	if (nn == NULL)
+		return (0);
 
-        while (tn != NULL)
-        {
-                if (strcmp(tn->key, key) == 0)
-                {
-                        free(tn->value);
-                        tn->value = strdup(value);
-                        return (1);
-                }
-                tn = tn->next;
-        }
-        nn = malloc(sizeof(hash_node_t));
-        if (nn == NULL)
-                return (0);
+	nn->key = strdup(key);
+	nn->value = strdup(value);
 
-        nn->key = strdup(key);
-        nn->value = strdup(value);
+	if (ht->array[idx] != NULL)
+	{
+	nn->next = ht->array[idx];
+	}
+	else
+	nn->next = NULL;
 
-        if (ht->array[idx] != NULL)
-        {
-                nn->next = ht->array[idx];
-        }
-        else
-                nn->next = NULL;
-
-        ht->array[idx] = nn;
-        return (1);
+	ht->array[idx] = nn;
+	return (1);
 }
